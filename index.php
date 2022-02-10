cf93956c9ea70d50a497fbfe312b24aaf0d08a4f
Maxym Berezynets <halustyan@ukr.net>
2022-02-10 19:02:47 +0200

    csv fixes 1.0

--- index.php
+++ index.php
@@ -9,5 +9,6 @@ use Htmlacademy\Models\CsvToSqlConverter;
 
 //$firstClass = new Task(2, 2);
 
+
 $csvConverterFirst = new CsvToSqlConverter('data/categories.xlsx');
 $csvConverterSecond = new CsvToSqlConverter('data/cities.xlsx');
