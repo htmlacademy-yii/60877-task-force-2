2e6386153b7eea57f49884c9462b0766950afcd2
Maxym Berezynets <halustyan@ukr.net>
2022-05-03 00:12:05 +0300

    Added GII

---
+++ models/Profiles.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "profiles".
+ *
+ * @property int $id
+ * @property string $address
+ * @property string $bd
+ * @property string $about
+ * @property string $phone
+ * @property string $skype
+ * @property string $city_id
+ */
+class Profiles extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'profiles';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['address', 'bd', 'about', 'phone', 'skype', 'city_id'], 'required'],
+            [['address', 'bd', 'about', 'phone', 'skype', 'city_id'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'address' => 'Address',
+            'bd' => 'Bd',
+            'about' => 'About',
+            'phone' => 'Phone',
+            'skype' => 'Skype',
+            'city_id' => 'City ID',
+        ];
+    }
+}
