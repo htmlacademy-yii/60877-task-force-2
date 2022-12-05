2effd56f861ad5a00a12d1c5e6a633d7dd04c430
halustyan <halustyan@ukr.net>
2022-12-05 23:12:11 +0200

    Добавил редирект на страницу задания

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -4,12 +4,9 @@ namespace app\controllers;
 
 use app\models\AddTask;
 use app\models\Category;
-use app\models\Files;
 use app\models\Replies;
 use app\models\SearchTasks;
 use app\models\Task;
-use Codeception\Step\Executor;
-use yii\base\BaseObject;
 use yii\web\NotFoundHttpException;
 use yii\filters\AccessControl;
 
@@ -32,11 +29,7 @@ class TasksController extends SecuredController
                         'allow' => false,
                         'actions' => ['add'],
                         'matchCallback' => function ($rule, $action) {
-                            $user = \Yii::$app->user->identity->user_status;
-
-                            $user_status = $user->user_status;
-                            $user_constant = User::EXECUTOR;
-                            return $user_status === $user_constant;
+                            return \Yii::$app->user->identity->user_status === User::EXECUTOR;
                         }
                     ]
                 ]
@@ -72,66 +65,15 @@ class TasksController extends SecuredController
 
     public function actionAdd()
     {
-
-
         $categories = Category::find()->all();
         $model = new AddTask();
 
-        if ($model->load(\Yii::$app->request->post()) && $model->validate()) {
 
-            /*начинаю сохранять в БД данные с формы*/
+    if ($model->load(\Yii::$app->request->post()) && $model->validate()) {
 
 
-            /*  $newTask->name = $model->about_job;
-
-              $newTask->description = $model->describe_task;
-              $newTask->category_id = $model->categories;
-              $newTask->address = $model->location; // в $model->location null
-
-              $newTask->budget = $model->budget;
-
-              $newTask->expire = $model->expire_date;
-              $newTask->create_at = date('Y-m-d h-m-s');
-              $newTask->status = 1;
-              $newTask->user_id = \Yii::$app->user->identity->id;
-
-              $newTask->save();
-
-              $instances = UploadedFile::getInstances($model, 'files');
-
-              foreach ($instances as $instance) {
-                  $files = new Files();
-                  $instance->saveAs("uploads/".$instance->name);
-                  $files->tasks_id = $newTask->id;
-                  $files->files_name = $instance->name;
-                  $files->save();
-              }*/
-            /*окончил сохранение*/
-            /*заканчиваю сохранять данные с формы */
-            $newTask = new Task();
-            $connection = $newTask->getDb();
-            $transaction = $connection->beginTransaction();
-            try {
-              $task_after_save =  $model->createNewTask();
-                $model->saveFile($task_after_save);
-
-                /*
-
-                 foreach ($instances as $instance) {
-                     $files = new Files();
-                     $instance->saveAs("uploads/" . $instance->name);
-                     $files->tasks_id = $newTask->id;
-                     $files->files_name = $instance->name;
-                     $files->save();
-                 }*/
-                $transaction->commit();
-            } catch (\Exception $e) {
-                $transaction->rollBack();
-                throw $e;
-            } catch (\Throwable $e) {
-                $transaction->rollBack();
-                throw $e;
-            }
+        $model->createNewTask();
+        \Yii::$app->response->redirect(['/tasks/view/', 'id' => $model->createNewTask()->id]);
         }
 
 
--- models/AddTask.php
+++ models/AddTask.php
@@ -2,9 +2,13 @@
 
 namespace app\models;
 
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
 use Yii;
 use yii\base\BaseObject;
 use yii\base\Model;
+use yii\db\Exception;
+use yii\web\NotFoundHttpException;
+use yii\web\ServerErrorHttpException;
 use yii\web\UploadedFile;
 use app\models\Task;
 use app\models\Files;
@@ -33,7 +37,8 @@ class AddTask extends Model
     public $expire_date;
     public $files;
     public $location;
-public $task_id;
+    public $task_id;
+
     /**
      * {@inheritdoc}
      */
@@ -51,54 +56,48 @@ public $task_id;
         ];
     }
 
-    /*public function upload()
-    {
-        if ($this->validate()) {
-            foreach ($this->files as $file) {
-                $file->saveAs('uploads/' . $file->baseName . '.' . $file->extension);
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }*/
-
     public function createNewTask()
     {
         $newTask = new Task();
         $newTask->name = $this->about_job;
-
         $newTask->description = $this->describe_task;
         $newTask->category_id = $this->categories;
         $newTask->address = $this->location; // в $model->location null
-
         $newTask->budget = $this->budget;
-
         $newTask->expire = $this->expire_date;
         $newTask->create_at = date('Y-m-d h-m-s');
         $newTask->status = 1;
         $newTask->user_id = \Yii::$app->user->identity->id;
-        $newTask->save();
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                if (!$newTask->save()){
+                    throw new Exception("Can not save new task");
+                }
+                $this->saveFile($newTask);
+
+                $transaction->commit();
+            } catch (\Exception $e) {
+                $transaction->rollBack();
+                throw $e;
+            } catch (\Throwable $e) {
+                $transaction->rollBack();
+                throw $e;
+            }
+
         return $newTask;
     }
 
     public function saveFile($task_after_save)
     {
-
         $instances = UploadedFile::getInstances($this, 'files');
-
         foreach ($instances as $instance) {
             $files = new Files();
-
             $instance->saveAs("uploads/" . $instance->name);
-
-
-
-
             $files->tasks_id = $task_after_save->id;
             $files->files_name = $instance->name;
-            $files->save();
-
+            if(!$files->save()) {
+                throw new ServerErrorHttpException("Файл(ы) не был(и) сохранен(ы)!");
+            }
         }
     }
 
